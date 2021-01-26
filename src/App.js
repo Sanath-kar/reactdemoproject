cbabf8b22881cce06f27417b18c7a7a9ef614177
Sanath S Karanth <sanathsk97@gmail.com>
2021-01-26 16:30:13 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -1,72 +1,135 @@
-import logo from './logo.svg';
+
 import React, { useState } from 'react';
 import './App.css';
 import 'react-widgets/dist/css/react-widgets.css';
-import { Multiselect } from 'react-widgets';
+// import { Multiselect } from 'react-widgets';
 
 
 function App() {
 
   // let { Multiselect } = ReactWidgets;
 
-  const [dropdata,setDropdata] = useState(['0-24', '25-99','100-249']);
+  const [dropdata,setDropdata] = useState([
+                                {
+                                  number:'0-24'
+                                },
+                                {
+                                  number:'25-99'
+                                },
+                                {
+                                  number:'100-249'
+                                },
+                                {
+                                  number:'250-499'
+                                },
+                                {
+                                  number:'500-999'
+                                },
+                                {
+                                  number:'1000-4999'
+                                },
+                                {
+                                  number:'5000-9999'
+                                },
+                                {
+                                  number:'10000-49999'
+                                },
+                                {
+                                  number:'50000-100000'
+                                },
+                                {
+                                  number:'>100000'
+                                },
+                                ]);
   const [selected,setSelected] = useState([]);
+  const [rightbox,setRightbox] = useState([]);
+  const [assign,setAssign] = useState([]);
+  const [btn,setBtn] = useState("id_button");
+  const [box,setBox] = useState("checkbox");
+  const [clear,setClear] = useState(false);
 
   const handleclick = () => {
-      selected.concat(dropdata);
-      setSelected(dropdata);
+    setRightbox(assign);
+    setClear(false);
+    console.log("kkkkkkkkkkkkkkkkkk"+ rightbox);
   }
   const selectall = () => {
-    setSelected(dropdata);
-    console.log("clicked------------------");
+    console.log("clicked");
+}
+const clearall = () => {
+  // setSelected(null);
+  setClear(true);
+  setRightbox([]);
+  
+  // setAssign(null);
+}
+const buttonclick = (btnvalues) => {
+  selected.push(btnvalues);
+  // selected.pop();
+  setAssign(selected);
 }
 
+
   return (
-    <div className="container-fluid mt-5">
+    <div className="container-fluid mt-4">
       <div className="row">
         <div className="col-6 d-inline">
-        <ul class="list-inline">
-            <li class="list-inline-item">Available: </li>
-            <li class="list-inline-item text-primary"><p onClick={selectall}>Select All</p></li>
+        <ul className="list-inline">
+            <li className="list-inline-item">Available: </li>
+            <li className="list-inline-item text-primary"><p onClick={selectall}>Select All</p></li>
         </ul>
         </div>
         <div className="col-6">
-        <ul class="list-inline">
-            <li class="list-inline-item">Selected: </li>
-            <li class="list-inline-item text-primary"><p onClick={console.log("clicked")}>Clear All</p></li>
+        <ul className="list-inline">
+            <li className="list-inline-item">Selected: </li>
+            <li className="list-inline-item text-primary"><p onClick={clearall}>Clear All</p></li>
         </ul>
         </div>
       </div>
       <div className="row">
+
         <div className="col-6">
-        <div class="border border-primary">
-          <Multiselect
-            data={dropdata}
-            defaultValue={['0-24', '25-99']}
-            textField='name'
-            caseSensitive={false}
-            minLength={3}
-            filter='contains'
-            onChange={handleclick}
-          />
-        </div>
-        {/* <div className="col-2">
-        </div> */}
-        </div>
-        <div className="col-6">
-        <div class="border border-primary">
-        <Multiselect
-            data={selected}
-            textField='name'
-            caseSensitive={false}
-            minLength={3}
-            filter='contains'
-          />
+        <div className="border border-primary bg-light">
+        <p id={btn}>
+        {dropdata.map((item, index) => (
+          <label>
+            <input key={index} 
+                   type={box} 
+                   onClick={() =>buttonclick(item.number)} 
+                   name="button1"
+                   value="1"></input>
+            <span>{item.number}</span>
+          </label>
+          ))}
+        </p>
         </div>
         </div>
+        <div className="mt-3">
+        <i className="fa fa-arrow-right" onClick={handleclick} aria-hidden="true"></i>
+        </div>
+        
+        
+        <div className="col-5">
+        <div className="border border-primary bgrightbox">
+        <p id="id_button">
+        {rightbox.map((item, index) => (
+          <label>
+            <input key={index} type="checkbox"  name="button1" value="1"></input>
+            <span>{item}</span>
+          </label>
+          ))}
+        </p>
+        { clear &&
+        <p>No data exist</p>
+        }
+        </div>
+        </div>
+
+        <div className="col-6"></div>
       </div>               
     </div>
   );
 }
+
 
 export default App;
